27ec673550b1a43e8da6df59240870e732da88b6
Simon Filipe <167369596+Simonfilipes@users.noreply.github.com>
2025-04-21 13:32:08 -0300

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const hero = () => {
   return (
-    <div className="bg-[url('bg.png')] bg-cover bg-center h-screen border-b-2 border-white"></div>
+    <div className="bg-[url('/bg.png')] bg-cover bg-center h-screen border-b-2 border-white"></div>
   )
 }
 
